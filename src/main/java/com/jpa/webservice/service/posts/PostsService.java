ece19c4ef674daf8d470342597b3c7d5fcfd25b6
82202354 <kkingkkang30@naver.com>
2023-03-07 17:58:23 +0900

    feat: post api

---
+++ src/main/java/com/jpa/webservice/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jpa.webservice.service.posts;
+
+import com.jpa.webservice.domain.posts.PostsRepository;
+import com.jpa.webservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
